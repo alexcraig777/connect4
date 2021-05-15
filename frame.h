7af55fb0911e9d341991453a1f797635fb27c27f
alexcraig777 <alexcraig777@gmail.com>
2021-05-15 12:02:38 -0400

    Updating frame.c and frame.h

--- frame.h
+++ frame.h
@@ -7,19 +7,10 @@ struct FramePosition* deep_copy_frame(struct FramePosition* template);
 
 void free_frame(struct FramePosition* frame);
 
-
 int get_to_move(struct FramePosition* frame);
 
 int get_at_col_row(struct FramePosition* frame, int col, int row);
 
 int move_in_col(struct FramePosition* frame, int col);
-
-void display_frame(struct FramePosition* position);
-
-int check_full(struct FramePosition* position);
-
-int check_winner(struct FramePosition* position);
-
-int score_frame(struct FramePosition* position, int player);
-
+  
 #endif
